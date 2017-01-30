7dbf10f34cabb27d80bb03e2bc1cdd09a1200989
kartik chauhan <chauhan.kartik25@gmail.com>
2017-01-31 03:45:26 +0530

    handling ajax request for login form done - unable to generate new token for new ajax request in case any error occurs

--- Login.php
+++ Login.php
@@ -16,45 +16,30 @@ if(Input::exists('post'))
 				'min' => 6
 				)
 			));
-
 		if($Validate->passed())
 		{
 			$user = new User;
 			if($user->login(Input::get('email'), Input::get('password'), Input::get('remember_me')))
-				Redirect::to('index.php');
+			{
+				$json['status'] = 0;	// status 0 => when successfully logged in
+				$json['message'] = 'successfully logged in';
+				// Redirect::to('index.php');
+			}
 			else
-				echo "Sorry wrong credentials";
+			{
+				$json['status'] = 1;	// status 1 => when credentials are wrong
+				$json['message'] = 'wrong credentials';
+				
+			}
 		}
 		else
 		{
-			foreach($Validate->errors() as $error)
-			{
-				echo $error.'<br>';
-			}
+			$json['status'] = 2;	// status 2 => if validation fails
+			$json['message'] = $Validate->errors()[0];
 		}
+		echo json_encode($json);
 	}
 }
 
+
 ?>
-
-
-<form action="" method="post">
-<div>
-	<label for="email">Email</label>
-	<input type="text" name="email" id="email" >
-</div>
-
-<div>
-	<label for="password">Password</label>
-	<input type="password" name="password" id="password" >
-</div>
-
-<div>
-	<label for="remember_me">
-		<input type="checkbox" name="remember_me" id="remember_me" >Remember me
-	</label>
-</div>
-<input type="hidden" name="_token" value="<?php echo Token::generate(); ?>">
-
-<input type="submit" value="login">
-</form>
--- login_frontend.php
+++ login_frontend.php
@@ -2,39 +2,6 @@
 
 require_once'Core/init.php';
 
-if(Input::exists('post'))
-{
-	if(Token::check(Input::get('_token')))
-	{
-		$Validate = new Validate;
-		$Validate->check($_POST, array(
-			'email' => array(
-				'required' => true
-				),
-			'password' => array(
-				'required' => true,
-				'min' => 6
-				)
-			));
-
-		if($Validate->passed())
-		{
-			$user = new User;
-			if($user->login(Input::get('email'), Input::get('password'), Input::get('remember_me')))
-				Redirect::to('index.php');
-			else
-				echo "Sorry wrong credentials";
-		}
-		else
-		{
-			foreach($Validate->errors() as $error)
-			{
-				echo $error.'<br>';
-			}
-		}
-	}
-}
-
 ?>
 
 <!Doctype html>
@@ -74,6 +41,10 @@ if(Input::exists('post'))
 				margin-top: 15px;
 				margin-bottom: 10px;
 			}
+			.error
+			{
+				display: none;
+			}
 		</style>
 
 	</head>
@@ -83,6 +54,9 @@ if(Input::exists('post'))
 			<h5 class="center-align condensed light">Sign in to BlogSparta</h5>
 			<div class="row">
 				<div class="col s4 offset-s4">
+					<ul class="collection center-align z-depth-1 error">
+						<li class="collection-item red-text"></li>
+					</ul>
 					<div class="card">
 						<div class="card-content">
 							<div class="row">
@@ -105,16 +79,16 @@ if(Input::exists('post'))
 										<div class="input-field col s12">
 											<input type="hidden" name="_token" id="_token" value="<?php echo Token::generate(); ?>">
 										</div>
-										<button class="btn waves-effect waves-light col s4 offset-s4" type="submit" value="submit">
-											Submit
-										</button>
+										<input type="submit" class="btn waves-effect waves-light col s4 offset-s4" value="submit" id="submit">
+											<!-- Submit
+										</button> -->
 									</div>
 								</form>
 							</div>
 						</div>
 					</div>
 					<div class="section">
-						<ul class="collection center-align z-depth-1" style="background-color: #eee">
+						<ul class="collection center-align z-depth-1">
 							<li class="collection-item">New to BlogSparta? <a href="register.php">Create an account</a></li>
 						</ul>
 					</div>
@@ -124,5 +98,44 @@ if(Input::exists('post'))
 		
 		<script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
 		<script type="text/javascript" src="Includes/js/materialize.min.js"></script>
+		<script type="text/javascript">
+			// $('#submit').off('click');
+			$(document).ready(function(){
+				$('body').on('click', '#submit', function(e){
+					e.preventDefault();
+					var email = $('#email').val();
+					var password = $('#password').val();
+					var remember_me = $('#remember_me').val();
+					var _token = $('#_token').val();
+					console.log(email);
+					console.log(password);
+					console.log(remember_me);
+					console.log(_token);
+					
+					$.ajax({
+						type : "POST",
+						url : "login.php",
+						data : {email: email, password: password, remember_me: remember_me, _token: _token},
+						cache: false,
+						success: function(response)
+						{
+							var response = JSON.parse(response);
+							if(response.status==0)
+							{
+								Materialize.toast("You've been logged in successfully", 4000, 'green');
+							}
+							else
+							{
+								Materialize.toast(response.message, 4000, 'red');
+								$('.error li').text(response.message);
+								$('.error').show();
+							}
+						}
+					});
+
+				});
+
+			});
+		</script>
 	</body>
 </html>
