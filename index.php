4d8a6d897841f893c808acd12741617b080cb66a
kartik chauhan <chauhan.kartik25@gmail.com>
2017-02-10 22:17:29 +0530

    tinymce editor added

--- index.php
+++ index.php
@@ -1,22 +1,33 @@
-<?php
-
-require_once'Core/init.php';
-
-$user = new User;
-
-if($user->isLoggedIn())
-{
-?>
-	Hii <a href="profile.php?user=<?php echo $user->data()->username ?>"><?php echo $user->data()->name ?></a>
-	<hr>
-	<a href="logout.php">Logout</a><hr>
-	<a href="update.php">Update Profile</a><hr>
-	<a href="changePassword.php">Change Password</a><hr>
-
-<?php
-}
-else
-{
-	echo 'not logged in';
-	// include_once'socialLogin.php';
-}
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+  <textarea>Wait your text editor is being loaded</textarea>
+  <script src="vendor/tinymce/tinymce/tinymce.min.js"></script>
+  <script>
+  tinymce.init({
+  selector: 'textarea',
+  height: 500,
+  theme: 'modern',
+  plugins: [
+    'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+    'searchreplace wordcount visualblocks visualchars code fullscreen',
+    'insertdatetime media nonbreaking save table contextmenu directionality',
+    'emoticons template paste textcolor colorpicker textpattern imagetools codesample toc'
+  ],
+  toolbar1: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+  toolbar2: 'print preview media | forecolor backcolor emoticons | codesample',
+  image_advtab: true,
+  templates: [
+    { title: 'Test template 1', content: 'Test 1' },
+    { title: 'Test template 2', content: 'Test 2' }
+  ],
+  content_css: [
+    '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
+    '//www.tinymce.com/css/codepen.min.css'
+  ]
+ });
+  </script>
+</body>
+</html>
