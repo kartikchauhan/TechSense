d28b06d341fc328b912c958d6d82515551193d09
kartik chauhan <chauhan.kartik25@gmail.com>
2017-02-18 08:06:40 +0530

    form data sent through ajax added

--- authors_info.php
+++ authors_info.php
@@ -12,7 +12,7 @@ require_once'Core/init.php';
 	</title>
 </head>
 <body>
-	<form action="authors_info_backend.php" method="post" enctype="multipart/form-data">
+	<form id="add_info" action="authors_info_backend.php" method="post" enctype="multipart/form-data">
 		<input type="file" name="profile_pic" id="profile_pic">
 		<br>
 		<label for="description">Write about yourself</label>
@@ -28,8 +28,38 @@ require_once'Core/init.php';
 		<input type="text" name="facebook_url" id="facebook_url">
 		<br>
 		<input type="hidden" name="_token" id="_token" value="<?php echo Token::generate(); ?>">
-		<input type="submit" value="submit">
+		<input type="submit" value="submit" id="submit">
 	</form>
+	<script src="Includes/js/jquery.min.js"></script>
+	<script type="text/javascript" src="Includes/js/materialize.min.js"></script>
+	<script type="text/javascript">
+		$("#submit").off('click');
+		$(document).ready(function(){
+				$('#submit').on('click', function(e){
+					e.preventDefault();
+					var data = new FormData();
+				    var file_data = $('input[type="file"]')[0].files;
+				    if(file_data.length)
+				    {
+				    	data.append("profile_pic", file_data[0]);
+				    }
+				    var input_data = $('form').serializeArray();
+				    $.each(input_data,function(key, input){
+				        data.append(input.name, input.value);
+				    });
+				    $.ajax({
+				        url: 'authors_info_backend.php',
+				        data: data,
+				        contentType: false,
+				        processData: false,
+				        type: 'POST',
+				        success: function(data){
+				            console.log(data);
+				        }
+				    });
+				});
+		});
+	</script>
 
 </body>
 </html>
--- authors_info_backend.php
+++ authors_info_backend.php
@@ -25,10 +25,10 @@ if(Input::exists())
 		if($Validate->passed())
 		{
 			$fields = array();
-			$fields['description'] = Input::get('description');
+			$fields['description'] = Input::get('description');		// add description to the array that is gonna be passed for the insert query
 			if(!empty(Input::get('github_url')))
 			{
-				$fields["github_url"] = Input::get('github_url');
+				$fields["github_url"] = Input::get('github_url');	// add github_url if user has entered github_url
 			}
 			if(!empty(Input::get('facebook_url')))
 			{
@@ -38,10 +38,10 @@ if(Input::exists())
 			{
 				$fields["twitter_url"] = Input::get("twitter_url");
 			}
-			if(!empty($_FILES["profile_pic"]["name"]))
+			if(!empty($_FILES))	// insert the data if no image uploaded
 			{
-				$target_dir = Config::get('url/upload_dir').'/';
-				$target_file = $target_dir . basename($_FILES["profile_pic"]["name"]);
+				$target_dir = Config::get('url/upload_dir').'/';	// target directory where images are gonna be stored
+				$target_file = $target_dir . basename($_FILES["profile_pic"]["name"]);	// basename of our file
 				$target_file_type = getimagesize($_FILES["profile_pic"]["tmp_name"])[2];	// getting file_type (extension too)
 			 	try
 				{
@@ -49,40 +49,43 @@ if(Input::exists())
 					{
 						throw new Exception("Uncompatible file extension. Only jpeg or png format files allowed");
 					}
-					echo "hey";
 					if(!move_uploaded_file($_FILES["profile_pic"]["tmp_name"], $target_file))
 					{
 						throw new Exception("couldn't process profile updation. Please try again later");
 					}
-					$fields["image_url"] = basename($_FILES["profile_pic"]["name"])	;
-
-					try 
-					{
-						if(!DB::getInstance()->insert('authors_info', $fields))
-							throw new Exception("Unable to insert values right now. Please try again later");
-						echo "record has been inserted";
-					}
-					catch(Exception $e)
-					{
-						echo $e->getMessage();
-					}
+					$fields["image_url"] = basename($_FILES["profile_pic"]["name"])	;	// creating index "image_url" if image uploaded is verified
+					insertData('authors_info', $fields);
 				}
 				catch(Exception $e)
 				{
 					echo $e->getMessage();
 				}
 			}
+			else
+			{
+				insertData('authors_info', $fields);	// insert the data even if there's no image
+			}
 		}
 		else
 		{
-			foreach ($Validate->errors() as $error)
-			{
-				echo $error."<br>";
-			}
+			echo $Validate->errors()[0];
 		}
 		
 	}
 }
 
+function insertData($table, $fields)
+{
+	try 
+	{
+		if(!DB::getInstance()->insert('authors_info', $fields))
+			throw new Exception("Unable to insert values right now. Please try again later");
+		echo "record has been inserted";
+	}
+	catch(Exception $e)
+	{
+		echo $e->getMessage();
+	}
+}
 
 ?>
