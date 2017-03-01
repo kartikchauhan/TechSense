f6bb062b71cfa5ea331296eb4144e1d491216a58
kartik chauhan <chauhan.kartik25@gmail.com>
2017-03-01 21:27:54 +0530

    delete and edit options removed for pagination in index.php - can't write blog if reloading authors_info.php - getting unexpected end of JSON input error

--- pagination_backend.php
+++ pagination_backend.php
@@ -29,7 +29,7 @@ if(Input::exists())
 		{
 			$blogs = DB::getInstance()->getRangeSort('blogs', $records_per_page, $offset, array('created_on', 'DESC'));	// get records in descending order within a certian range based upon the offset and records_per_page values
 		}
-		if($blogs->count())
+		if($blogs->count() && Input::get('author'))
 		{
 			$blogs = $blogs->results();
 			$json['content'] = '';	// content will hold the html 
@@ -50,6 +50,56 @@ if(Input::exists())
 	                            	<h5><a class='views' data-attribute='{$blog->views}' href='".Config::get('url/endpoint')."/view_blog.php?blog_id={$blog->id}'".">".ucfirst($blog->title)."</a></h5>
 	                            </div>
 	                            	<a href='#' class='blue-text delete-blog' data-attribute='{$blog->id}'><i class='material-icons right'>delete</i></a> <a href='update_blog.php?blog_id={$blog->id}' class='blue-text edit-blog' data-attribute='{$blog->id}'><i class='material-icons right'>mode_edit</i></a> 
+	                        </div>
+                			<h6>".ucfirst($blog->description)."</h6><br>
+			                <div class='row'>
+			                    <div class='measure-count' data-attribute='{$blog->id}'>
+			                        <div class='col s1'>
+			                            <i class='fa fa-eye fa-2x' aria-hidden='true' style='color:grey'></i>
+			                        </div>
+			                        <div class='col s1'>
+			                            {$blog->views}
+			                        </div>
+			                        <div class='col s1 offset-s1'>
+			                            <i class='fa fa-thumbs-up fa-2x' aria-hidden='true' style='color:grey'></i>
+			                        </div>
+			                        <div class='col s1'>
+			                            {$blog->likes}
+			                        </div>
+			                        <div class='col s1 offset-s1'>
+			                            <i class='fa fa-thumbs-down fa-2x' aria-hidden='true' style='color:grey'></i>
+			                        </div>
+			                        <div class='col s1'>
+			                            {$blog->dislikes}
+			                        </div>
+			                    </div>
+			                </div>
+			                <div class='divider'></div>
+			            </div>
+			        </div>";
+			}
+		}
+		else if($blogs->count() && !Input::get('author'))
+		{
+			
+			$blogs = $blogs->results();
+			$json['content'] = '';	// content will hold the html 
+			foreach($blogs as $blog)
+			{
+				$date=strtotime($blog->created_on);
+				$json['content'] = $json['content'].
+					"<div class='row blog'>
+			            <div class='col s2'>
+			                <blockquote>".
+			                    date('M', $date)."<br>".
+			                    date('Y d', $date).
+			                "</blockquote>
+			            </div>
+			            <div class='col s10'>
+		            		<div class='row'>
+	                        	<div class='col s4'>
+	                            	<h5><a class='views' data-attribute='{$blog->views}' href='".Config::get('url/endpoint')."/view_blog.php?blog_id={$blog->id}'".">".ucfirst($blog->title)."</a></h5>
+	                            </div>
 	                        </div>
                 			<h6>".ucfirst($blog->description)."</h6><br>
 			                <div class='row'>
