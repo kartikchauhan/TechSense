3be2ca89d8e477a16ad7ac5b012cf55bc52967e4
kartik chauhan <chauhan.kartik25@gmail.com>
2017-04-06 04:24:27 +0530

    response code added for query type 'title' in pagination.php - need to write code for query types 'name' and 'tags'

--- pagination_backend.php
+++ pagination_backend.php
@@ -165,11 +165,13 @@ if(Input::exists())
 		}
 		else if(empty(Input::get('author')) && (Input::get('pagination_item') == 1 || Input::get('pagination_item') == 2 || Input::get('pagination_item') == 3 || Input::get('pagination_item') == 4))
 		{
+			$json['content'] = '';
 			$search = DB::getInstance();
 			$query = Input::get('query');
 			$query = split(":", $query);
 			$query_field = strtolower(trim($query[0]));
 			$query_field_value = strtolower(trim($query[1]));
+
 			if($query_field === 'user')
 			{
 				$result = $search->searchIdViaUser('users', array('username', '=', $query_field_value));		// fetching the id of the user
@@ -194,76 +196,37 @@ if(Input::exists())
 						throw new Exception("There are no blogs associated to User ".$query_field_value);
 					}
 					$resultBlogs = $resultBlogs->results();
-					$json['content'] = '';
-					foreach($resultBlogs as $blog)
-					{
-						$blog_tags = DB::getInstance()->get('blog_tags', array('blog_id', '=', $blog->id));
-		                $blog_tags = $blog_tags->results();
-						$date=strtotime($blog->created_on);
-						$json['content'] = $json['content'].
-							"<div class='pagination_item_value' data-attribute='1'></div>
-							<div class='row blog'>
-					            <div class='col s12 hide-on-med-and-up'>
-		                            <div class='col s6'>
-		                                <blockquote>".
-		                                    date('M d', $date).' '.
-		                                    date('Y', $date).
-		                                "</blockquote>
-		                            </div>
-		                        </div>
-		                        <div class='col s2 l2 hide-on-small-only'>
-		                            <blockquote>".
-		                                date('M', $date)."<br>".
-		                                date('Y d', $date).
-		                            "</blockquote>
-		                        </div>
-					            <div class='col s12 l10'>
-				            		<div class='row'>
-			                        	<div class='col s12 l10'>
-			                            	<h5><a class='views' data-attribute='{$blog->views}' href='".Config::get('url/endpoint')."/view_blog.php?blog_id={$blog->id}'".">".ucfirst($blog->title)."</a></h5>
-		                					<h6>".ucfirst($blog->description)."</h6><br>
-			                            </div>
-			                        </div>
-					                <div class='row'>
-					                    <div class='measure-count' data-attribute='{$blog->id}'>
-					                        <div class='col s2 l1'>
-					                            <i class='fa fa-eye fa-2x' aria-hidden='true' style='color:grey'></i>
-					                        </div>
-					                        <div class='col s1 l1'>
-					                            {$blog->views}
-					                        </div>
-					                        <div class='col s2 l1 offset-s1 offset-l1'>
-					                            <i class='fa fa-thumbs-up fa-2x' aria-hidden='true' style='color:grey'></i>
-					                        </div>
-					                        <div class='col s1 l1'>
-					                            {$blog->likes}
-					                        </div>
-					                        <div class='col s2 l1 offset-s1 offset-l1'>
-					                            <i class='fa fa-thumbs-down fa-2x' aria-hidden='true' style='color:grey'></i>
-					                        </div>
-					                        <div class='col s1 l1'>
-					                            {$blog->dislikes}
-					                        </div>
-					                    </div>
-					                </div>";
-		                            foreach($blog_tags as $blog_tag)
-		                            {
-		                                $json['content'] = $json['content'].
-		                                "<div class='chip'>".$blog_tag->tags."</div>";
-		                            }
-		                            $json['content'] = $json['content'].
-					                "<div class='divider'></div>
-					            </div>
-					        </div>";
-					}
+					addHtmlToResponse($json, $resultBlogs);		// add HTML content to the resonse
+					
 				}
 				catch(Exception $e)
 				{
-					$json['content'] = 
-						"<div class='pagination_item_value' data-attribute='1'></div>
-						<div class='section'>
-							<h6 class='center'>".$e->getMessage()."</h6>
-						</div>";
+					addErrorToResponse($json, $e->getMessage());
+				}
+			}
+			else if($query_field === 'title')
+			{
+				$query_field_value = '%'.$query_field_value.'%';
+
+				$resultBlogs = $search->searchBlogsViaTitle('blogs', array('title', 'like', $query_field_value), array('created_on', 'DESC'), $records_per_page, $offset);
+				try
+				{
+					if(!$resultBlogs)
+					{
+						throw new Exception("Some error occured while fetching results. Please try again later");
+					}
+					if($resultBlogs->count() == 0)
+					{
+						$query_field_value = substr($query_field_value, 1, -1);		// striping first and last character wiz. '%'
+						throw new Exception("There are no blogs associated with the title ".$query_field_value);
+					}
+					$resultBlogs = $resultBlogs->results();
+					addHtmlToResponse($json, $resultBlogs);
+
+				}
+				catch(Exception $e)
+				{
+					addErrorToResponse($json, $e->getMessage());
 				}
 			}
 		}
@@ -286,6 +249,80 @@ else
 	Redirect::to('index.php');
 }
 
+function addHtmlToResponse($json, $resultBlogs)
+{
+	global $json;
+	foreach($resultBlogs as $blog)
+	{
+		$blog_tags = DB::getInstance()->get('blog_tags', array('blog_id', '=', $blog->id));
+	    $blog_tags = $blog_tags->results();
+		$date=strtotime($blog->created_on);
+		$json['content'] = $json['content'].
+			"<div class='pagination_item_value' data-attribute='1'></div>
+			<div class='row blog'>
+	            <div class='col s12 hide-on-med-and-up'>
+	                <div class='col s6'>
+	                    <blockquote>".
+	                        date('M d', $date).' '.
+	                        date('Y', $date).
+	                    "</blockquote>
+	                </div>
+	            </div>
+	            <div class='col s2 l2 hide-on-small-only'>
+	                <blockquote>".
+	                    date('M', $date)."<br>".
+	                    date('Y d', $date).
+	                "</blockquote>
+	            </div>
+	            <div class='col s12 l10'>
+	        		<div class='row'>
+	                	<div class='col s12 l10'>
+	                    	<h5><a class='views' data-attribute='{$blog->views}' href='".Config::get('url/endpoint')."/view_blog.php?blog_id={$blog->id}'".">".ucfirst($blog->title)."</a></h5>
+	    					<h6>".ucfirst($blog->description)."</h6><br>
+	                    </div>
+	                </div>
+	                <div class='row'>
+	                    <div class='measure-count' data-attribute='{$blog->id}'>
+	                        <div class='col s2 l1'>
+	                            <i class='fa fa-eye fa-2x' aria-hidden='true' style='color:grey'></i>
+	                        </div>
+	                        <div class='col s1 l1'>
+	                            {$blog->views}
+	                        </div>
+	                        <div class='col s2 l1 offset-s1 offset-l1'>
+	                            <i class='fa fa-thumbs-up fa-2x' aria-hidden='true' style='color:grey'></i>
+	                        </div>
+	                        <div class='col s1 l1'>
+	                            {$blog->likes}
+	                        </div>
+	                        <div class='col s2 l1 offset-s1 offset-l1'>
+	                            <i class='fa fa-thumbs-down fa-2x' aria-hidden='true' style='color:grey'></i>
+	                        </div>
+	                        <div class='col s1 l1'>
+	                            {$blog->dislikes}
+	                        </div>
+	                    </div>
+	                </div>";
+	                foreach($blog_tags as $blog_tag)
+	                {
+	                    $json['content'] = $json['content'].
+	                    "<div class='chip'>".$blog_tag->tags."</div>";
+	                }
+	                $json['content'] = $json['content'].
+	                "<div class='divider'></div>
+	            </div>
+	        </div>";
+	}
+}
 
+function addErrorToResponse($json, $errorMessage)
+{
+	global $json;
+	$json['content'] = 
+		"<div class='pagination_item_value' data-attribute='1'></div>
+		<div class='section'>
+			<h6 class='center'>".$errorMessage."</h6>
+		</div>";
+}
 
 ?>
