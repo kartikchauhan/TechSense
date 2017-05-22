d1c76bf2a62bd2a07c5b93cd935178d0f986426d
kartik chauhan <chauhan.kartik25@gmail.com>
2017-05-23 05:19:34 +0530

    material-icons added in search.php and histogram.php

--- histogram.php
+++ histogram.php
@@ -24,7 +24,9 @@
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
-    <title>Histogram layout of tags</title>
+    <title>Histogram layout of count of tags</title>
+    
+    <link href="//fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel='stylesheet' href='Includes/css/histogram.css'>
 </head>
 <body>
--- search.php
+++ search.php
@@ -32,7 +32,7 @@ else
 	<head>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 		<title>Search Results for <?php Input::get('searchParameter'); ?></title>
-		<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+		<link href="//fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/css/materialize.min.css">
     	<style type="text/css">
     		body
