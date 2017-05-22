165bb0ed202373285f25efdeb3c75af632efdc17
kartik chauhan <chauhan.kartik25@gmail.com>
2017-05-23 04:53:43 +0530

    credentials modified of google analytics reporting API - new account linked

--- view_blog.php
+++ view_blog.php
@@ -195,13 +195,13 @@ function updateViews($reports, $blog, $blogId)
 <html>
 	<head>
 		<script>
-		  	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-		  	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-		  	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		  	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  				(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  				m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  			})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-		  	ga('create', 'UA-98214883-3', 'auto');
-		  	ga('send', 'pageview');
+  			ga('create', 'UA-98214883-5', 'auto');
+  			ga('send', 'pageview');
 
 		</script>
 
