c2f779c3fadcf4eea289b5419c72ea1e92a2e7b6
kartik chauhan <chauhan.kartik25@gmail.com>
2017-05-23 01:02:19 +0530

    redirect url changed

--- Includes/googleAuth/gpConfig.php
+++ Includes/googleAuth/gpConfig.php
@@ -9,7 +9,7 @@ include_once 'src/contrib/Google_Oauth2Service.php';
  */
 $clientId = '285926229424-cm218npu455mta48b8r6uq4nassnedvj.apps.googleusercontent.com'; //Google client ID
 $clientSecret = '2iQeZP1nxnGKoAAEnRUDMVmi'; //Google client secret
-$redirectURL = 'https://techsense.herokuapp.com/login.php'; //Callback URL
+$redirectURL = 'https://techwit.herokuapp.com/login.php'; //Callback URL
 
 //Call Google API
 $gClient = new Google_Client();
